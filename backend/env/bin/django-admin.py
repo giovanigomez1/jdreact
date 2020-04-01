f678a37746badbcb2e03c95da1a40f9a85e3edb6
Giovani Gomez <mhq2230@gmail.com>
2020-03-31 22:11:13 -0600

    project started, api created and working

---
+++ backend/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Applications/XAMPP/xamppfiles/htdocs/varios/pract/django/djreact/backend/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
